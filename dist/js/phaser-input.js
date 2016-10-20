541589d50040ef577ecfb40a83884e71617cc9ba
squilibob <squilibob@excite.com>
2016-10-20 16:53:55 +1000

    phaser-input keeps zooming in

--- dist/js/phaser-input.js
+++ dist/js/phaser-input.js
@@ -30,6 +30,7 @@ var Fabrique;
             this.element.style.position = 'absolute';
             this.element.style.top = (-100).toString() + 'px';
             this.element.style.left = (-100).toString() + 'px';
+            this.font: '16px Arial';
             this.element.value = this.value;
             this.element.type = InputType[type];
             this.element.addEventListener('focusin', function () {
