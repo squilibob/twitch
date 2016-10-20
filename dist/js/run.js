9e893ffe2dc5b0adb330a3e84d6c67bad2774ce0
squilibob <squilibob@excite.com>
2016-10-20 17:45:04 +1000

    phaser-input keeps zooming in

--- dist/js/run.js
+++ dist/js/run.js
@@ -73,7 +73,7 @@ project.Init.prototype = {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
-    game.scale.setScreenSize(true);
+    game.scale.refresh();
     pokeball_loader();
     var script;
     for (testscript in preloadscripts) {
