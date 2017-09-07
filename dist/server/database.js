1e8c57d774fd43311cb579eafaee9681ad565d4a
squilibob <squilibob@excite.com>
2017-09-07 21:34:35 +1000

    clean up socket code

--- dist/server/database.js
+++ dist/server/database.js
@@ -210,7 +210,7 @@ exports.saveuserpokes = function(r, conn, name, payload) {
        })
   .run(conn)
   .catch(error => reject(error))
-}
+ }
 
 exports.validatefc = function(r, conn, username) {
   let starter = [191, 298, 401, 010, 013, 265, 280, 129, 349, 664, 011, 014, 172, 266, 268, 174, 194, 236, 665, 161, 173, 261, 270, 273, 440, 412]
@@ -238,8 +238,119 @@ exports.manuallyenterraffle = function(r, conn, username, displayicon) {
     .catch(error => reject(error))
   })
 }
-// exports.getavatar = function(r, conn, username) {
-//   return new Promise(function(resolve, reject) {
-//   })
-// }
+
+exports.sendraffleupdate = function(r, conn) {
+  return new Promise(function(resolve, reject) {
+    r.db('Users').table('Raffle')
+    .run(conn)
+    .then(cursor => cursor.toArray())
+    .then(result => { resolve(current) })
+    .catch(error => reject(error))
+  })
+}
+
+exports.senduserpokes = function(r, conn, username) {
+  return new Promise(function(resolve, reject) {
+    r.table('Users').filter(r.row('id').eq(username.toLowerCase()))
+    .without('pokevalues')
+    .getField('active')
+    .run(conn)
+    .then(active => {
+      if (err || active[0] == undefined || active == [] || active == -1) reject(err ? err : active)
+      else {
+        r.table('Users').filter(r.row('id').eq(username.toLowerCase()))
+        .without('pokevalues')
+        .getField('teams')
+        .run(conn)
+        .then(cursor => cursor.toArray())
+        .then(result => { resolve({user: username, name: active, team: result[0][active]}) })
+      }
+    })
+    .catch(error => reject(error))
+  })
+}
+
+exports.newuser = function(payload) {
+  r.table('Users').get(payload.id.toLowerCase())
+  .run(conn, function(err, cursor) {
+    if (!cursor || err) createanewuser(payload)
+    else {
+      r.table('Users').get(payload.id).update({
+        ign: payload.ign,
+        fc: payload.fc
+      }).run(conn, function(err, result) {
+        if (err) throw err
+        if (result.errors) console.log(result.first_error)
+      })
+    }
+  })
+}
+
+exports.createanewuser = function(r, conn, payload, firstcard) {
+  return new Promise(function(resolve, reject) {
+        r.table('Users').insert(payload).run(conn, function(err, result) {
+          if (err) throw err
+          if (result.errors) console.log(result.first_error)
+          else {
+            r.table('Users').get(payload.id).update({
+              validated: false,
+              cards: [{'poke': firstcard, 'level': 1}],
+              active: "default",
+              avatar: -1,
+              teams: {
+                  "default": [
+                      0 ,
+                      1 ,
+                      2 ,
+                      3 ,
+                      4 ,
+                      5
+                  ]}
+            }).run(conn, function(err, result) {
+              if (err) throw err
+              if (result.errors) console.log(result.first_error)
+              // else socket.emit('login accepted', result[0])
+            })
+            database.raffleChangeUser(conn, payload.id.toLowerCase(), 12, true, firstcard, "default", [
+                      0 ,
+                      1 ,
+                      2 ,
+                      3 ,
+                      4 ,
+                      5
+                  ])
+            resolve(payload.id)
+            // socket.broadcast.emit('someone signed up',payload.id)
+          }
+        })
+  })
+}
+
+exports.newuser = function(r, conn, payload) {
+    r.table('Users').get(payload.id.toLowerCase())
+    .run(conn, function(err, cursor) {
+      if (!cursor || err) createanewuser(r, conn, payload) //////////////fix
+      else {
+        r.table('Users').get(payload.id.toLowerCase()).update({
+          ign: payload.ign,
+          fc: payload.fc
+        }).run(conn, function(err, result) {
+          if (err) throw err
+          if (result.errors) console.log(result.first_error)
+        })
+      }
+    })
+}
+
+
+
+exports.requesttoconnect = function(r, conn, username) {
+  return new Promise(function(resolve, reject) {
+    r.table('Users').filter(r.row('id').eq(username))
+    .run(conn)
+    .then(cursor => cursor.toArray())
+    .then(result => { resolve(result[0]) })
+    .catch(error => reject(error))
+  })
+}
 
--- dist/server/listeners.js
+++ dist/server/listeners.js
@@ -7,247 +7,101 @@ module.exports = function(expressServer) {
   socket = expressServer.socket
   dbcall = expressServer.dbcall
 
-    socket.on('disconnect', function(){
-      console.log('user disconnected', socket.id)
-    })
-    socket.on('request to connect', function(msg){
-      // if (msg.id != '' && msg.id != undefined && msg.pokevalues[0] > 0 && msg.pokevalues[1] > 0 && msg.pokevalues[2] > 0) {
-      if (msg.id != '' && msg.id != undefined) {
-        console.log('user: ' + msg.id, 'connecting...')
-        r.table('Users').filter(r.row('id').eq(msg.id.toLowerCase()))
-        .run(conn, function(err, cursor) {
-          if (err) createanewuser(msg)
-          else cursor.toArray(function(err, result) {
-            if (err || result[0] == undefined || result == []) createanewuser(msg)
-            else {
-              // if (result[0].pokevalues[0] == msg.pokevalues[0] && result[0].pokevalues[1] == msg.pokevalues[1] && result[0].pokevalues[2] == msg.pokevalues[2]) {
-                socket.emit('login accepted',result[0])
-              // }
-              // else socket.emit('wrong password')
-            }
-          })
-        })
+  socket.on('request to connect', async function(msg){
+    if (msg.id != '' && msg.id != undefined) {
+      console.log('user: ' + msg.id, 'connecting...')
+      let connected = await dbcall.requesttoconnect(r, conn, msg.id.toLowerCase())
+      if (!connected) {
+        createanewuser(msg)
       } else {
-        socket.emit('user not found')
-        console.log('undefined value sent', msg)
+        socket.emit('login accepted', connected)
       }
-    })
-
-    // socket.on('log chat', function(payload){
-    //  fs.appendFile('public/chatlog.html', '<li>'  + payload + '<li>\n', (err) => {
-    //    if (err) console.log(err)
-    //  })
-    // })
-
-    function createanewuser(payload) {
-      var starter = [198, 313, 314, 546, 661, 300, 431, 509, 677, 52, 352, 335, 619, 86, 283, 211, 296, 615, 165 , 167, 88]
-      // if (payload.id != '' && payload.id != undefined && payload.pokevalues[0] > 0 && payload.pokevalues[1] > 0 && payload.pokevalues[2] > 0 && payload.fc[0] >0 && payload.fc[0] < 10000 && payload.fc[1] >0 && payload.fc[1] < 10000  && payload.fc[2] >0 && payload.fc[2] < 10000  && payload.ign != '' && payload.ign != undefined) {
-      if (payload.id != '' && payload.id != undefined && payload.fc[0] >0 && payload.fc[0] < 10000 && payload.fc[1] >0 && payload.fc[1] < 10000  && payload.fc[2] >0 && payload.fc[2] < 10000  && payload.ign != '' && payload.ign != undefined) {
-        // payload.id = payload.id.split(' ')[0]
-        payload.id = payload.id.trim()
-        r.table('Users').insert(payload).run(conn, function(err, result) {
-          if (err) throw err
-          if (result.errors) console.log(result.first_error)
-          else {
-            firstcard = starter[Math.floor(Math.random()*starter.length)]
-            r.table('Users').get(payload.id).update({
-              validated: false,
-              cards: [{'poke': fir, 'level': 1}],
-              active: "default",
-              avatar: -1,
-              teams: {
-                  "default": [
-                      0 ,
-                      1 ,
-                      2 ,
-                      3 ,
-                      4 ,
-                      5
-                  ]}
-            }).run(conn, function(err, result) {
-              if (err) throw err
-              if (result.errors) console.log(result.first_error)
-              // else socket.emit('login accepted', result[0])
-            })
-            io.emit('someone signed up',payload.id)
-            database.raffleChangeUser(conn, payload.id.toLowerCase(), 12, true, firstcard, "default", [
-                      0 ,
-                      1 ,
-                      2 ,
-                      3 ,
-                      4 ,
-                      5
-                  ])
-            // socket.broadcast.emit('someone signed up',payload.id)
-          }
-        })
-      } else console.log('user not created, there was missing information')
+    } else {
+      console.log('undefined value sent', msg)
     }
+  })
+  // socket.on('log chat', function(payload){
+  //  fs.appendFile('public/chatlog.html', '<li>'  + payload + '<li>\n', (err) => {
+  //    if (err) console.log(err)
+  //  })
+  // })
+  socket.on('disconnect', () => console.log('user disconnected', socket.id))
+  socket.on('new user', payload => dbcall.newuser(r, conn, payload))
+  socket.on("send fusion", (firstpoke, secondpoke) => io.emit('show fusion', firstpoke, secondpoke))
+  socket.on('update avatar', (username, newavatar)  => dbcall.updateavatar(r, conn, username, newavatar))
+  socket.on('update badge', (username, newbadge) => dbcall.updatebadge(r, conn, username, newbadge))
+  socket.on('set current team', (name, payload) =>  dbcall.setcurrentteam(r, conn, name, payload))
+  socket.on('save user pokes', (name, payload) =>  dbcall.saveuserpokes(r, conn, name, payload))
+  socket.on('metaphone', (meta, message) => socket.emit('texttopika', meta, message))
+  socket.on('enter raffle', (username, displayicon, team_name, team) => database.raffleChangeUser(conn, username.toLowerCase(), 12, true, displayicon, team_name, team))
+  socket.on('leave raffle', (username, displayicon, team_name) => database.raffleChangeUser(conn, username.toLowerCase(), 12, false, displayicon, '', [0]))
+  socket.on("pokemon cry", poke => io.emit("playsound", ('000' + poke).substr(-3)))
+  socket.on("Vote poll", payload => dbcall.votepoll(r, conn, payload))
+  socket.on("Send vote", payload => dbcall.sendvote(r, conn, payload))
+  socket.on("Show vote", () => socket.emit("Vote options", result))
+  socket.on('request user pokes', username => sendUserPokes(username))
+  socket.on('validate fc', username =>  dbcall.validatefc(r, conn, username))
+  socket.on('followed', person => socket.emit('new follower', person))
+  socket.on('won raffle', person => database.rafflewinner(conn, person))
+  socket.on('clear raffle', ()  => dbcall.clearraffle())
+  socket.on('send raffle', needupdate => sendRaffleUpdate(needupdate))
+  socket.on('send emote', payload => io.emit('receive emote', payload))
+  socket.on('update leaderboard', entry => dbcall.updateleaderboard(r, conn, entry))
+  socket.on('create new player', payload => socket.emit('receive new player', payload))
+  socket.on('clear leaderboard', () => dbcall.clearleaderboard(r, conn))
+  socket.on ("Ask for pokedex", function(){ socket.emit("Receive pokedex", expressServer.cache.pokedex) }) //replace with Ask for table
+  socket.on ("Ask for typechart", function(){ socket.emit("Receive typechart", expressServer.cache.typechart) })//replace with Ask for table
+  socket.on ("Ask for table", async function(dbname) { socket.emit("receive " + await dbname.toLowerCase(), expressServer.cached[dbname]) })
+  socket.on("update vote", async function(){ io.emit('receive vote', await dbcall.gettable(r, conn, 'Vote')) })
+  socket.on('Request vote', async function(){ io.emit('receive vote', await dbcall.gettable(r, conn, 'Vote')) })
+  socket.on('request user fc', async function(username) { socket.emit('user fc', await dbcall.getfc(r, conn, username.toLowerCase())) })
+  socket.on('request avatar', async function(channel, user, message, self) { socket.emit('receive avatar', channel, user, message, self, await dbcall.getavatar(r, conn, user.username.toLowerCase())) })
+  socket.on('request badge', async function(user) { socket.emit('receive badge', username, await dbcall.getbadge(user.username.toLowerCase())) })
+  socket.on('manually enter raffle', async function(username, displayicon) { database.raffleChangeUser(conn, username.toLowerCase(), 12, true, await dbcall.manuallyenterraffle(r, conn, username, displayicon)) })
+  socket.on('manually leave raffle', async function(username, displayicon) { database.raffleChangeUser(conn, username.toLowerCase(), 12, false, await dbcall.manuallyenterraffle(r, conn, username, displayicon)) })
+  socket.on('send leaderboard', async function(){ io.emit('receive leaderboard', await dbcall.gettable(r, conn, 'Leaderboard')) })
 
-    socket.on('new user', function(payload){
-      r.table('Users').get(payload.id.toLowerCase())
-      .run(conn, function(err, cursor) {
-        if (!cursor || err) createanewuser(payload)
-        else {
-          r.table('Users').get(payload.id).update({
-            ign: payload.ign,
-            fc: payload.fc
-          }).run(conn, function(err, result) {
-            if (err) throw err
-            if (result.errors) console.log(result.first_error)
-          })
-        }
-      })
-    })
+  socket.on ("Insert pokedex", async function(payload){  //all these need to be a single socket call like Ask for table
+    console.log(payload["id"],payload["Pokemon"])
+    let result = await dbcall.put(r, conn, 'Pokedex', payload)
+    console.log(JSON.stringify(result, null, 2))
+  })
 
-    socket.on ("Ask for pokedex", function(){ socket.emit("Receive pokedex", expressServer.cache.pokedex) }) //replace with Ask for table
+  socket.on ("Insert ability", function(payload){//all these need to be a single socket call like Ask for table
+    let result = dbcall.put(r, conn, 'Abilities', payload)
+    console.log(JSON.stringify(result, null, 2))
+  })
 
-    socket.on ("Ask for typechart", function(){ socket.emit("Receive typechart", expressServer.cache.typechart) })//replace with Ask for table
+  socket.on ("Insert move", function(payload){//all these need to be a single socket call like Ask for table
+    let result = dbcall.put(r, conn, 'Moves', payload)
+    console.log(JSON.stringify(result, null, 2))
+  })
 
-    socket.on ("Ask for table", (dbname) => socket.emit("receive " + dbname.toLowerCase(), expressServer.cached[dbname]))
+  socket.on ("Insert bttv", function(payload){//all these need to be a single socket call like Ask for table
+    let result = dbcall.put(r, conn, 'Bttv', payload)
+    console.log(JSON.stringify(result, null, 2))
+  })
 
-    socket.on ("Insert pokedex", async function(payload){  //all these need to be a single socket call like Ask for table
-      console.log(payload["id"],payload["Pokemon"])
-      let result = expressServer.dbcall.put(r, conn, 'Pokedex', payload)
-      console.log(JSON.stringify(result, null, 2))
-    })
+  socket.on ("Insert ffz", function(payload){//all these need to be a single socket call like Ask for table
+    let result = dbcall.put(r, conn, 'Ffz', payload)
+    console.log(JSON.stringify(result, null, 2))
+  })
 
-    socket.on ("Insert ability", function(payload){//all these need to be a single socket call like Ask for table
-      let result = expressServer.dbcall.put(r, conn, 'Abilities', payload)
-      console.log(JSON.stringify(result, null, 2))
-    })
+  async function sendUserPokes (username) { io.emit('user pokes', await dbcall.senduserpokes(username)) }
 
-    socket.on ("Insert move", function(payload){//all these need to be a single socket call like Ask for table
-      let result = expressServer.dbcall.put(r, conn, 'Moves', payload)
-      console.log(JSON.stringify(result, null, 2))
-    })
-
-    socket.on ("Insert bttv", function(payload){//all these need to be a single socket call like Ask for table
-      let result = expressServer.dbcall.put(r, conn, 'Bttv', payload)
-      console.log(JSON.stringify(result, null, 2))
-    })
-
-    socket.on ("Insert ffz", function(payload){//all these need to be a single socket call like Ask for table
-      let result = expressServer.dbcall.put(r, conn, 'Ffz', payload)
-      console.log(JSON.stringify(result, null, 2))
-    })
-
-    socket.on("pokemon cry", function(poke){ io.emit("playsound", ('000' + poke).substr(-3)) })
-
-    socket.on("update vote", async function(){ io.emit('receive vote', await expressServer.dbcall.gettable(r, conn, 'Vote')) })
-
-    socket.on("Vote poll", payload => expressServer.dbcall.votepoll(r, conn, payload))
-
-    socket.on("Send vote", payload => expressServer.dbcall.sendvote(r, conn, payload))
-
-    socket.on("Show vote", () => socket.emit("Vote options", result))
-
-    socket.on("send fusion", (firstpoke, secondpoke) => io.emit('show fusion', firstpoke, secondpoke))
-
-    socket.on('Request vote', async function(){ io.emit('receive vote', await expressServer.dbcall.gettable(r, conn, 'Vote')) })
-
-    socket.on('request user fc', async function(username) { socket.emit('user fc', await dbcall.getfc(r, conn, username.toLowerCase())) })
-
-    socket.on('request avatar', async function(channel, user, message, self) { socket.emit('receive avatar', channel, user, message, self, await dbcall.getavatar(r, conn, user.username.toLowerCase())) })
-
-    socket.on('request badge', async function(user) { socket.emit('receive badge', username, await dbcall.getbadge(user.username.toLowerCase())) })
-
-    socket.on('update avatar', (username, newavatar)  => expressServer.dbcall.updateavatar(r, conn, username, newavatar))
-
-    socket.on('update badge', (username, newbadge) => expressServer.dbcall.updatebadge(r, conn, username, newbadge))
-
-    socket.on('request user pokes', function(username) { sendUserPokes(username) })
-
-    // socket.on('request unvalidated', function() {
-    //   r.table('Users')
-    //   .filter({validated: false})
-    //   .run(conn, function(err, cursor) {
-    //     cursor.toArray(function(err, result) {
-    //       if (err || result[0] == undefined || result == []) console.log('user not found')
-    //       else {
-    //         socket.emit('unvalidated users', result)
-    //       }
-    //     })
-    //   })
-    // })
-
-    socket.on('set current team', (name, payload) =>  expressServer.dbcall.setcurrentteam(r, conn, name, payload))
-
-    socket.on('save user pokes', (name, payload) =>  expressServer.dbcall.saveuserpokes(r, conn, name, payload))
-
-    socket.on('validate fc', (username) =>  expressServer.dbcall.validatefc(r, conn, username))
-
-    socket.on('followed', function(person) { socket.emit('new follower', person) })
-
-    socket.on('metaphone', function(meta, message) { socket.emit('texttopika', meta, message) })
-
-    socket.on('won raffle', function(person) { database.rafflewinner(conn, person) })
-
-    socket.on('manually enter raffle', async function(username, displayicon) { database.raffleChangeUser(conn, username.toLowerCase(), 12, true, await expressServer.dbcall.manuallyenterraffle(r, conn, username, displayicon)) })
-
-    socket.on('manually leave raffle', async function(username, displayicon) { database.raffleChangeUser(conn, username.toLowerCase(), 12, false, await expressServer.dbcall.manuallyenterraffle(r, conn, username, displayicon)) })
-
-    socket.on('enter raffle', function(username, displayicon, team_name, team) { database.raffleChangeUser(conn, username.toLowerCase(), 12, true, displayicon, team_name, team) })
-
-    socket.on('leave raffle', function(username, displayicon, team_name) { database.raffleChangeUser(conn, username.toLowerCase(), 12, false, displayicon, '', [0]) })
-
-    socket.on('clear raffle', ()  => expressServer.dbcall.clearraffle())
-
-    socket.on('send raffle', function(needupdate) { sendRaffleUpdate(needupdate) })
-
-    socket.on('send emote', function(payload) {
-      io.emit('receive emote', payload)
-      // socket.broadcast.emit('receive emote', payload)
-      socket.emit('receive emote', payload)
-    })
-
-    socket.on('update leaderboard', entry => expressServer.dbcall.updateleaderboard(r, conn, entry))
-
-    socket.on('create new player', function(payload) { socket.emit('receive new player', payload) })
-
-
-    socket.on('send leaderboard', async function(){ io.emit('receive leaderboard', await expressServer.dbcall.gettable(r, conn, 'Leaderboard')) })
-
-    socket.on('clear leaderboard', () => expressServer.dbcall.clearleaderboard(r, conn))
-
-  function sendUserPokes (username) {
-    r.table('Users').filter(r.row('id').eq(username.toLowerCase()))
-    .without('pokevalues')
-    .getField('active')
-    .run(conn, function(err, active) {
-      if (err || active[0] == undefined || active == [] || active == -1) io.emit('no user active team found')
-      else if (result) {
-        r.table('Users').filter(r.row('id').eq(username.toLowerCase()))
-        .without('pokevalues')
-        .getField('teams')
-        .run(conn, function(err, cursor) {
-          cursor.toArray(function(err, result) {
-            if (err || result[0] == undefined || result == []) io.emit('user not found')
-            else {
-              io.emit('user pokes', {user: username, name: active, team: result[0][active]})
-            }
-          })
-        })
-      }
-    })
+  async function sendRaffleUpdate(updated){
+    let current = await dbcall.sendraffleupdate(r, conn)
+    io.emit('receive raffle', current)
+    if (updated) io.emit('raffle update', current)
   }
 
-
-
-  function sendRaffleUpdate(updated){
-    var current = []
-    r.db('Users').table('Raffle')
-      .run(conn, function(err, cursor) {
-        cursor.toArray(function(err, result) {
-          if (err) console.log('error not found')
-          else {
-            if (result[0] == undefined || result == []) current = []
-            else current = result
-            io.emit('receive raffle', current)
-            if (updated) io.emit('raffle update', current)
-          }
-        })
-      })
+  async function createanewuser(payload) {
+    let starter = [198, 313, 314, 546, 661, 300, 431, 509, 677, 52, 352, 335, 619, 86, 283, 211, 296, 615, 165 , 167, 88]
+    // if (payload.id != '' && payload.id != undefined && payload.pokevalues[0] > 0 && payload.pokevalues[1] > 0 && payload.pokevalues[2] > 0 && payload.fc[0] >0 && payload.fc[0] < 10000 && payload.fc[1] >0 && payload.fc[1] < 10000  && payload.fc[2] >0 && payload.fc[2] < 10000  && payload.ign != '' && payload.ign != undefined) {
+    if (payload.id != '' && payload.id != undefined && payload.fc[0] >0 && payload.fc[0] < 10000 && payload.fc[1] >0 && payload.fc[1] < 10000  && payload.fc[2] >0 && payload.fc[2] < 10000  && payload.ign != '' && payload.ign != undefined) {
+      // payload.id = payload.id.split(' ')[0]
+      payload.id = payload.id.trim()
+      io.emit('someone signed up', await dbcall.createanewuser(r, conn, payload, starter[Math.floor(Math.random()*starter.length)]))
+    } else console.log('user not created, there was missing information')
   }
-
 }
